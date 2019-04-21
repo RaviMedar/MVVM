94b1d57ca92a2b3b385a10495d462bc9fd10baf4
Ravi <rmeda@allstate.com>
2019-04-21 23:46:27 +0530

    Modified Test Packages

--- app/src/androidTest/java/com/ravilearning/mvvm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ravilearning/mvvm/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.itomkinas.blog_mvvm;
+package com.ravilearning.mvvm;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -7,7 +7,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.itomkinas.blog_mvvm", appContext.getPackageName());
+        assertEquals("com.ravilearning.mvvm", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/ravilearning/mvvm/ExampleUnitTest.java
+++ app/src/test/java/com/ravilearning/mvvm/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.itomkinas.blog_mvvm;
+package com.ravilearning.mvvm;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
